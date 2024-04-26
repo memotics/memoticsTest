ec4db1aa44ca8bd89adf83fe5412d892642e351c
memotics <134905785+memotics@users.noreply.github.com>
2024-04-26 09:58:43 +0800

    build test

--- src/VirtualTour/Context.js
+++
@@ -1,6 +0,0 @@
-import { createContext } from "react";
-
-export const StoredData = createContext({
-  dropPhysics: false, // Default for dropPhysics
-  RenderingSpace: "CloudSpace", // Default for RenderingSpace
-});
